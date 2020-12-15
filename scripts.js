014e52e55487bf3b177eb5a77c9a226041d5598c
Cameron Erdman <cmerdman@outlook.com>
2020-12-14 21:58:55 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -30,15 +30,15 @@ function showSlides(n) {
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const house = { lat: 40.802815, lng: -77.679779 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("maps"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 17,
+      center: house,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: house,
       map: map,
     });
   }
